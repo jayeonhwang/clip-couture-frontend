241df9bdfa1aaa28c07cf7787c5782eb8a8dd506
Jayeon Hwang <jayeon512@gmail.com>
2024-02-11 20:19:06 -0600

    init and create index and show actions

---
+++ src/Content.jsx
@@ -0,0 +1,51 @@
+import { ProductsIndex } from "./ProductsIndex";
+import { ProductsNew } from "./ProductsNew";
+import { ProductsShow } from "./ProductsShow"
+import axios from "axios";
+import { useState, useEffect } from "react"
+import { Modal } from "./Modal";
+
+export function Content() {
+  const [products, setProducts] = useState([]);
+  const [isProductsShowVisible, setProductsShowVisible] = useState(false)
+  const [currentProduct, setCurrentProduct] = useState({})
+
+  const getProducts = () => {
+    axios.get("http://localhost:3000/products.json").then(response => {
+      setProducts(response.data);
+    })
+  }
+
+  const createProduct = (params, successCallBack) => {
+    axios.post("http://localhost:3000/products.json", params).then(
+      response => {
+        setProducts([...products, response.data])
+        successCallBack();
+      })
+  }
+
+  const handleShowProduct = (product) => {
+    console.log("handleClose")
+    setProductsShowVisible(true)
+    setCurrentProduct(product)
+  }
+
+  const handleClose = () => {
+    setProductsShowVisible(false)
+  }
+
+  useEffect(getProducts, []);
+
+  return (
+
+    <main>
+      <h1>Clip-Couture</h1>
+      <ProductsNew onCreateProduct={createProduct} />
+      <ProductsIndex products={products} onShowProduct={handleShowProduct} />
+      <Modal show={isProductsShowVisible} onClose={handleClose}>
+        <ProductsShow product={currentProduct} />
+      </Modal>
+
+    </main>
+  )
+}
---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,17 @@
+export function ProductsIndex(props) {
+
+
+  return (
+    <div>
+      <h1>All products</h1>
+      {props.products.map(product => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.price}</p>
+          <button onClick={() => props.onShowProduct(product)}>More info</button>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
