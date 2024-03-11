99eeef861c3a393deb76bb6fcd6c6bb211fdd13a
Jayeon Hwang <jayeon512@gmail.com>
2024-03-11 15:51:41 -0500

    add dropdown option for create product

--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export function ProductsNew(props) {
   const [images, setImages] = useState([''])
+  const [suppliers, setSuppliers] = useState([])
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target)
     props.onCreateProduct(params, () => event.target.reset())
   }
 
+  const suppliersIndex = () => {
+    axios.get(`http://localhost:3000/suppliers.json`).then(response => {
+      console.log(response.data)
+      setSuppliers(response.data)
+    })
+  }
+
+
   const addImage = () => {
     setImages([...images, ''])
   }
+
+  useEffect(suppliersIndex, [])
+
 
   return (
     <div>
@@ -25,12 +39,20 @@ export function ProductsNew(props) {
         <div>
           Description: <input name="description" type="integer" />
         </div>
-        <div>
-          supplier_id: <input name="supplier_id" type="integer" />
-        </div>
+
         {images.map(image => (
           <p>Image:<input name="images[]" type="text" /></p>
         ))}
+
+        <div>
+          supplier:
+          <select name="supplier">
+            {suppliers.map(supplier => (
+              <option>{supplier.name}</option>
+            ))}
+          </select>
+        </div>
+
         <button type="submit">Create Product</button>
       </form>
 
