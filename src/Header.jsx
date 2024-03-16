b517513eff424d3ca329ef34173a957023ea1cd9
Jayeon Hwang <jayeon512@gmail.com>
2024-03-16 16:02:15 -0500

    add cart and logout link

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
+import { LogoutLink } from "./LogoutLink"
 
 export function Header() {
   return (
     <header>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/new">New</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Login</Link>
+        <Link to="/">Home</Link> | <Link to="/new">New</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Login</Link> | <Link to="/cart">Cart</Link> | <LogoutLink />
       </nav>
     </header>
   )
