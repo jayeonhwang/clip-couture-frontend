583a139d9297b17d0d5d144192df67b20c082eed
Jayeon Hwang <jayeon512@gmail.com>
2024-04-22 16:55:20 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 import axios from 'axios'
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://intense-harbor-19462-bb10365960de.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://clip-couture-backend.onrender.com";
+// "https://intense-harbor-19462-bb10365960de.herokuapp.com/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
